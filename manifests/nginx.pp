7bae9398144a1233696c5112f9e8cf5b1c65931f
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2014-10-02 03:14:18 +0200

    nginx config

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -2,6 +2,12 @@ class textgrid::nginx {
 
     package { 'nginx': ensure  => present; }
 
+	service { 'nginx':
+    	ensure  => running,
+    	enable  => true,
+    	require => Package["nginx"],
+	}
+
 	file { '/etc/nginx/proxyconf':
         ensure  => directory,
         owner   => root,
@@ -17,5 +23,14 @@ class textgrid::nginx {
         mode    => '0644',
         content => template('textgrid/etc/nginx/proxyconf/1.0.conf.erb'),
     }
+
+	file { '/etc/nginx/sites-available/default':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        content => template('textgrid/etc/nginx/sites-available/default.erb'),
+    }
+
 
 }
