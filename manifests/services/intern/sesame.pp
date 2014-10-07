6f917cb9026cbb33fcb61319a634b12f9c1c175f
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2014-10-07 14:30:16 +0200

    sesame fix

--- manifests/services/intern/sesame.pp
+++ manifests/services/intern/sesame.pp
@@ -14,9 +14,7 @@ class textgrid::services::intern::sesame {
   $control_port = '9006'
   $jmx_port = '9991'
 
-  # http://sourceforge.net/projects/sesame/files/Sesame%202/2.7.13/openrdf-sesame-2.7.13-sdk.tar.gz/download
   $sesame_version = '2.7.13'
-  #$sesame_version = '2.7.8'
   $sesame_file = "openrdf-sesame-${sesame_version}-sdk.tar.gz"
 
   textgrid::resources::servicetomcat { $tgname:
@@ -25,7 +23,6 @@ class textgrid::services::intern::sesame {
     http_port    => $http_port,
     control_port => $control_port,
     jmx_port     => $jmx_port,
-    #notify       => [create_tgrepo{'textgrid-nonpublic'],create_tgrepo{'textgrid-public']],
   }
 
 #  staging::deploy { $sesame_file:
@@ -45,44 +42,8 @@ class textgrid::services::intern::sesame {
     war_ensure    => present,
     catalina_base => "/home/${tgname}/${tgname}",
     war_source    => "http://central.maven.org/maven2/org/openrdf/sesame/sesame-http-server/${sesame_version}/sesame-http-server-${sesame_version}.war",
-    require       => [Textgrid::Resources::Servicetomcat[$tgname]],
+    require       => [Textgrid::Resources::Servicetomcat[$tgname],Tomcat::War['openrdf-workbench.war']],
   }
-
-#    file { "/home/${tgname}/repo1.trig":
-#        ensure  => present,
-#        owner   => $tgname,
-#        group   => $tgname,
-#        mode    => '644',
-#        source  => "puppet:///modules/textgrid/rdf/repo1.trig",
-#    }
-
-#	exec { 'create_sesame_repos':
-#		path    => ['/usr/bin','/bin','/usr/sbin'],
-#		command => "curl -X POST -d @/home/tomcat-sesame/repo1.trig http://localhost:9091/openrdf-sesame/repositories/SYSTEM/statements --header 'Content-Type: application/x-trig;charset=UTF-8'",
-#		require => File["/home/${tgname}/repo1.trig"],
-#	}
-
-#	exec { 'create_sesame_public_repo':
-#		path    => ['/usr/bin','/bin','/usr/sbin'],
-#		command => "curl 'http://localhost:9091/openrdf-workbench/repositories/SYSTEM/create' --data 'type=native&Repository+ID=textgrid-public&Repository+title=rdf+repo+for+public+textgrid+data&Triple+indexes=spoc%2Cposc%2Copsc%2Csopc'",
-#	}
-
-#	exec { 'mime_to_sesame_public_repo':
-#		path    => ['/usr/bin','/bin','/usr/sbin'],
-#		command => "curl 'http://localhost:9091/openrdf-sesame/repositories/textgrid-public/statements' -X POST -d @/home/tomcat-sesame/mime.ttl --header 'Content-Type: text/turtle;charset=UTF-8'",
-#		require => [Exec['create_sesame_public_repo'],File["/home/${tgname}/mime.ttl"]],
-#	}
-
-#	exec { 'create_sesame_nonpublic_repo':
-#		path    => ['/usr/bin','/bin','/usr/sbin'],
-#		command => "curl 'http://localhost:9091/openrdf-workbench/repositories/SYSTEM/create' --data 'type=native&Repository+ID=textgrid-nonpublic&Repository+title=rdf+repo+for+nonpublic+textgrid+data&Triple+indexes=spoc%2Cposc%2Copsc%2Csopc'",
-#	}
-
-#	exec { 'mime_to_sesame_nonpublic_repo':
-#		path    => ['/usr/bin','/bin','/usr/sbin'],
-#		command => "curl 'http://localhost:9091/openrdf-sesame/repositories/textgrid-nonpublic/statements' -X POST -d @/home/tomcat-sesame/mime.ttl --header 'Content-Type: text/turtle;charset=UTF-8'",
-#		require => [Exec['create_sesame_nonpublic_repo'],File["/home/${tgname}/mime.ttl"]],
-#	}
 
   file { "/home/${tgname}/mime.ttl":
     ensure => present,
@@ -91,6 +52,28 @@ class textgrid::services::intern::sesame {
     mode   => '0644',
     source => 'puppet:///modules/textgrid/rdf/mime.ttl',
   }
+
+#  unless $sesame_nonpublic_repo_created {
+    create_tgrepo{'textgrid-nonpublic':
+      port    => '9091',
+      user    => $tgname,
+      require => [Tomcat::War['openrdf-sesame.war'],Tomcat::War['openrdf-workbench.war']],
+    }
+#    file {'/etc/facter/facts.d/sesame_nonpublic.txt':
+#      content => "sesame_nonpublic_repo_created=true",
+#    }
+#  }
+
+#  unless $sesame_public_repo_created {
+    create_tgrepo{'textgrid-public':
+      port => '9091',
+      user => $tgname,
+      require => [Tomcat::War['openrdf-sesame.war'],Tomcat::War['openrdf-workbench.war']],
+    }
+#    file {'/etc/facter/facts.d/sesame_public.txt':
+#      content => "sesame_public_repo_created=true",
+#    }
+#  }
 
   define create_tgrepo (
     $port = '9091',
@@ -115,37 +98,5 @@ class textgrid::services::intern::sesame {
 
   }
 
-  unless $sesame_nonpublic_repo_created {
-    create_tgrepo{'textgrid-nonpublic':
-      port    => '9091',
-      user    => $tgname,
-  #    creates => "/home/$tgname/.aduna/openrdf-sesame/repositories/textgrid-nonpublic",
-      require => [Tomcat::War['openrdf-sesame.war'],Tomcat::War['openrdf-workbench.war']],
-    }
-    file {'/etc/facter/facts.d/sesame_nonpublic.txt':
-      content => "sesame_nonpublic_repo_created=true",
-    }
-  }
-
-  unless $sesame_public_repo_created {
-    create_tgrepo{'textgrid-public':
-      port => '9091',
-      user => $tgname,
-      require => [Tomcat::War['openrdf-sesame.war'],Tomcat::War['openrdf-workbench.war']],
-    }
-    file {'/etc/facter/facts.d/sesame_public.txt':
-      content => "sesame_public_repo_created=true",
-    }
-  }
-
-  # add tg repos
-  # todo: how to test for already done
-  # todo: better readable command exec
-  #	exec {'create_public_sesame_repos':
-  #		path    => ['/usr/bin','/bin','/usr/sbin'],
-  #		command => "echo -e 'connect http://localhost:9091/openrdf-sesame.\ncreate native.\ntextgrid-public\nrdf repo for public textgrid data\nspoc,posc,opsc,sopc\nopen textgrid-public.\nload /home/${tgname}/mime.ttl.\nexit.' | /home/${tgname}/openrdf-sesame-${sesame_version}/bin/console.sh",	
-  #        creates => "/home/${tgname}/.aduna/",
-  #		require => [Staging::Deploy[$sesame_file], File["/home/${tgname}/mime.ttl"]],
-  #	}
 
 }
