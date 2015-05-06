d588e4c181594fd16a55b258a6cbff9d4ca0e0dc
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2015-05-06 22:03:04 +0200

    symlinks for nfs-data

--- manifests/services/intern/datadirs.pp
+++ manifests/services/intern/datadirs.pp
@@ -2,7 +2,11 @@
 #
 # Class to install and configure TG-cruds data folders.
 #
-class textgrid::services::intern::datadirs {
+class textgrid::services::intern::datadirs (
+  $create_local_datadirs = true,
+  $data_public_location = '',
+  $data_nonpublic_location = '',
+){
 
   ###
   # the data dir
@@ -25,32 +29,43 @@ class textgrid::services::intern::datadirs {
   #require => [File['/mnt/storage'],Package['nfs-common']],
   #}
 
-  file { '/data/public':
-    ensure => directory,
-    owner  => 'textgrid',
-    group  => 'ULSB',
-    mode   => '0755',
-  }
+  if($create_local_datadirs) {
+    file { '/data/public':
+      ensure => directory,
+      owner  => 'textgrid',
+      group  => 'ULSB',
+      mode   => '0755',
+    }
 
-  file { '/data/nonpublic':
-    ensure  => directory,
-    owner   => 'textgrid',
-    group   => 'ULSB',
-    mode    => '0755',
-  }
+    file { '/data/nonpublic':
+      ensure  => directory,
+      owner   => 'textgrid',
+      group   => 'ULSB',
+      mode    => '0755',
+    }
 
-  file { '/data/public/productive':
-    ensure  => directory,
-    owner   => 'textgrid',
-    group   => 'ULSB',
-    mode    => '0755',
-  }
+    file { '/data/public/productive':
+      ensure  => directory,
+      owner   => 'textgrid',
+      group   => 'ULSB',
+      mode    => '0755',
+    }
 
-  file { '/data/nonpublic/productive':
-    ensure  => directory,
-    owner   => 'textgrid',
-    group   => 'ULSB',
-    mode    => '0755',
+    file { '/data/nonpublic/productive':
+      ensure  => directory,
+      owner   => 'textgrid',
+      group   => 'ULSB',
+      mode    => '0755',
+    }
+  } else {
+    file { '/data/public':
+      ensure => 'link',
+      target => $data_public_location
+    }
+    file { '/data/nonpublic':
+      ensure => 'link',
+      target => $data_nonpublic_location
+    }  
   }
 
 }
