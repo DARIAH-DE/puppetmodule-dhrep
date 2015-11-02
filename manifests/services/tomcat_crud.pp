f9161c2486909f8d78d92646474b06fc985a9f18
Stefan E. Funk <funk@sub.uni-goettingen.de>
2015-11-02 14:06:43 +0100

    mc

--- manifests/services/tomcat_crud.pp
+++ manifests/services/tomcat_crud.pp
@@ -3,9 +3,9 @@
 # Class to install and configure dhcrud or tgcrud tomcat.
 #
 class dhrep::services::tomcat_crud (
+  $scope        = undef,
 ){
 
-  $scope        = 'textgrid'
   $short        = 'tgcrud'
   $catname      = 'tomcat-tgcrud'
   $http_port    = '9093'
@@ -15,7 +15,7 @@ class dhrep::services::tomcat_crud (
   $jmx_port     = '9993'
   $gid          = '29900'
   $uid          = '49628'
-  $template     = "${scope}/etc/default/tomcat.${short}.erb"
+  $template     = "dhrep/etc/default/tomcat.${short}.erb"
   $depcat       = 'wildfly'
 
   $user         = $scope
