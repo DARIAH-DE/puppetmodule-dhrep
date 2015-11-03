d80bb8c13ac99a7e91356a8ec36a1950f16e9705
Stefan E. Funk <funk@sub.uni-goettingen.de>
2015-11-03 16:41:17 +0100

    Added pp files for textgridlab_org.

---
+++ manifests/static/textgridlab_org.pp
@@ -0,0 +1,44 @@
+# == Class: dhrep::static::textgridlab_org
+#
+# Class to install and configure the textgridlab.org static sites
+#
+class dhrep::static::textgridlab_org(
+) {
+
+  include dhrep::tgnginx
+
+  Exec {
+    path => ['/usr/bin','/bin','/usr/sbin'],
+  }
+
+  #
+  # Create folder
+  #
+  file { '/var/www/nginx-root/textgridlab.org':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
+  ->
+
+  #
+  # Clone metadata schemas
+  #
+  exec { 'git_clone_textgridlab_metadata':
+    command => 'git clone git://git.projects.gwdg.de/textgrid-metadata.git /var/www/nginx-root/textgridlab.org/schema',
+    creates => '/var/www/nginx-root/textgridlab.org/schema',
+    require => Package['git'],
+  }
+#  ->
+#  file { '/var/www/nginx-root/textgridlab.org/':
+#    source  => 'file:///var/www/nginx-root/textgridrep.de/textgridrep-webseite',
+#    recurse => true,
+#  }
+#  ->
+#  file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite/sandbox':
+#    ensure => link,
+#    target => '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox',
+#  }
+
+}
