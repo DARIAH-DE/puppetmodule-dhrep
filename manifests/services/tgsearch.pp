ab484f3019bc08e1bb4c4d1221ed7a1a7486bcf2
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2015-11-03 14:53:12 +0100

    tgsearch from .deb

--- manifests/services/tgsearch.pp
+++ manifests/services/tgsearch.pp
@@ -3,12 +3,16 @@
 # Class to install and configure tgsearch.
 #
 class dhrep::services::tgsearch (
+  $scope            = 'textgrid',
   $short            = 'tgsearch',
   $tgsearch_name    = 'tgsearch-nonpublic-webapp',
-  $tgsearch_version = '3.4.0-SNAPSHOT',
-  $tgsearch_group   = 'info.textgrid.middleware',
-  $maven_repository = 'http://dev.dariah.eu/nexus/content/repositories/snapshots/',
+  $tgsearch_version = 'latest',
 ) {
+
+  package { $tgsearch_name:
+    ensure  => $tgsearch_version,
+    require => Exec['update_dariah_ubunturepository'],
+  }
 
   include dhrep::services::tomcat_tgsearch
 
@@ -45,36 +49,9 @@ class dhrep::services::tgsearch (
     require => File["/etc/textgrid/${short}"],
   }
 
-  ###
-  # use maven to fetch latest tgsearch service from nexus, copy war, set permissions,
-  # and restart tomcat tgsearch.
-  ###
-
-  maven { "/var/cache/textgrid/${tgsearch_name}-${tgsearch_version}.war":
-    ensure     => latest,
-    groupid    => $tgsearch_group,
-    artifactid => $tgsearch_name,
-    version    => $tgsearch_version,
-    packaging  => 'war',
-    repos      => $maven_reposirory,
-    require    => Package['maven'],
-    notify     => Exec['replace_tgsearch_service'],
-  }
-
-  exec { 'replace_tgsearch_service':
-    path        => ['/usr/bin','/bin'],
-    command     => "/etc/init.d/${catname} stop && rm -rf /home/${catname}/${catname}/webapps/${short} && sleep 2 && cp /var/cache/textgrid/${tgsearch_name}-${tgsearch_version}.war /home/${catname}/${catname}/webapps/${short}.war",
-    cwd         => '/root',
-    user        => 'root',
-    group       => 'root',
-    require     => Exec["create_${catname}"],
-    refreshonly => true,
-  }
-  ->
-  file { "/home/${catname}/${catname}/webapps/${short}.war":
-    group  => $group,
-    mode   => '0640',
-    notify => Service[$catname],
+  file { "/home/${catname}/${catname}/webapps/${short}.war": 
+    ensure => 'link',
+    target => "/var/${scope}/webapps/tgsearch-nonpublic.war", 
   }
 
 }
--- manifests/services/tgsearch_public.pp
+++ manifests/services/tgsearch_public.pp
@@ -3,12 +3,16 @@
 # Class to install and configure tgsearch public.
 #
 class dhrep::services::tgsearch_public (
+  $scope            = 'textgrid',
   $short            = 'tgsearch-public',
   $tgsearch_name    = 'tgsearch-public-webapp',
-  $tgsearch_version = '3.4.0-SNAPSHOT',
-  $tgsearch_group   = 'info.textgrid.middleware',
-  $maven_repository = 'http://dev.dariah.eu/nexus/content/repositories/snapshots/',
+  $tgsearch_version = 'latest',
 ) {
+
+  package { $tgsearch_name:
+    ensure  => $tgsearch_version,
+    require => Exec['update_dariah_ubunturepository'],
+  }
 
   include dhrep::services::tomcat_tgsearch
 
@@ -45,36 +49,9 @@ class dhrep::services::tgsearch_public (
     require => File["/etc/textgrid/${short}"],
   }
 
-  ###
-  # use maven to fetch latest tgsearch service from nexus, copy war, set permissions,
-  # and restart tomcat tgsearch.
-  ###
-
-  maven { "/var/cache/textgrid/${tgsearch_name}-${tgsearch_version}.war":
-    ensure     => latest,
-    groupid    => $tgsearch_group,
-    artifactid => $tgsearch_name,
-    version    => $tgsearch_version,
-    packaging  => 'war',
-    repos      => $maven_repository,
-    require    => Package['maven'],
-    notify     => Exec['replace_tgsearch_service_public'],
-  }
-
-  exec { 'replace_tgsearch_service_public':
-    path        => ['/usr/bin','/bin'],
-    command     => "/etc/init.d/${catname} stop && rm -rf /home/${catname}/${catname}/webapps/${short} && sleep 2 && cp /var/cache/textgrid/${tgsearch_name}-${tgsearch_version}.war /home/${catname}/${catname}/webapps/${short}.war",
-    cwd         => '/root',
-    user        => 'root',
-    group       => 'root',
-    require     => Exec["create_${catname}"],
-    refreshonly => true,
-  }
-  ->
-  file { "/home/${catname}/${catname}/webapps/${short}.war":
-    group  => $group,
-    mode   => '0640',
-    notify => Service[$catname],
+  file { "/home/${catname}/${catname}/webapps/${short}.war": 
+    ensure => 'link',
+    target => "/var/${scope}/webapps/tgsearch-public.war", 
   }
 
 }
