f49d11786be5cc8e9bbcc1f77426c33c804910c3
Stefan E. Funk <funk@sub.uni-goettingen.de>
2015-11-05 13:39:50 +0100

    Added some mpore static data.

--- manifests/static/textgridlab_org.pp
+++ manifests/static/textgridlab_org.pp
@@ -30,9 +30,15 @@ class dhrep::static::textgridlab_org(
     creates => '/var/www/nginx-root/textgridlab.org/schema',
     require => Package['git'],
   }
+  ->
+  file { '/var/www/nginx-root/textgridlab.org/schema':
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
 
   #
-  # TODO middleware and puppet documentation
+  # TODO Automated middleware and puppet documentation!
   #
   file { '/var/www/nginx-root/textgridlab.org/doc':
     ensure  => directory,
@@ -40,47 +46,74 @@ class dhrep::static::textgridlab_org(
     group   => root,
     mode    => '0755',
   }
-  ->
   file { '/var/www/nginx-root/textgridlab.org/doc/index.html':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/doc/index.html',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/doc/index.html',
+    mode    => '0644',
+    require => File['/var/www/nginx-root/textgridlab.org/doc'],
   }
 
   #
-  # TODO marketplace
+  # TODO Marketplace
   #
 
   #
-  # index.html, favicon.ico, jira.html, error sites, and TODO images
+  # HTML files: index.html, favicon.ico, jira.html, error sites, and images
   #
   file { '/var/www/nginx-root/textgridlab.org/index.html':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/index.html',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/index.html',
+    mode    => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
   }
   file { '/var/www/nginx-root/textgridlab.org/favicon.ico':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/favicon.ico',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/favicon.ico',
+    mode    => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
   }
   file { '/var/www/nginx-root/textgridlab.org/jira.html':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/jira.html',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/jira.html',
+    mode    => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
   }
   file { '/var/www/nginx-root/textgridlab.org/404.html':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/404.html',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/404.html',
+    mode    => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
   }
   file { '/var/www/nginx-root/textgridlab.org/50x.html':
-    source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/50x.html',
-    mode   => '0644',
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/50x.html',
+    mode    => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
+  }
+  file { '/var/www/nginx-root/textgridlab.org/img':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/var/www/nginx-root/textgridlab.org'],
+  }
+  file { '/var/www/nginx-root/textgridlab.org/img/Buch-Eule.png':
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/img/Buch-Eule.png',
+    mode    => '0644',
+    require => File['/var/www/nginx-root/textgridlab.org/img'],
+  }
+  file { '/var/www/nginx-root/textgridlab.org/img/Drunk-Eule.png':
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/img/Drunk-Eule.png',
+    mode    => '0644',
+    require => File['/var/www/nginx-root/textgridlab.org/img'],
+  }
+  file { '/var/www/nginx-root/textgridlab.org/img/Release-Eule.png':
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/img/Release-Eule.png',
+    mode    => '0644',
+    require => File['/var/www/nginx-root/textgridlab.org/img'],
+  }
+  file { '/var/www/nginx-root/textgridlab.org/img/Schraub-Eule.png':
+    source  => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/img/Schraub-Eule.png',
+    mode    => '0644',
+    require => File['/var/www/nginx-root/textgridlab.org/img'],
   }
 
   #
-  # repstatus
+  # Repstatus
   #
   file { '/var/www/nginx-root/textgridlab.org/repstatus.html':
     source => 'puppet:///modules/dhrep/var/www/nginx-root/textgridlab.org/repstatus.html',
@@ -92,13 +125,13 @@ class dhrep::static::textgridlab_org(
     mode   => '0644',
     require => File['/var/www/nginx-root/textgridlab.org'],
   }
-      
+
   #
-  # TODO update site
+  # TODO Update site
   #
-  
+
   #
   # TODO TG-lab download files
   #
-    
+
 }
