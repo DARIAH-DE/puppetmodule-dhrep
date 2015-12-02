01bf99342c867d135936a7e23f2c19fd414817c3
Stefan E. Funk <funk@sub.uni-goettingen.de>
2015-12-02 18:31:52 +0100

    Added tivoli script to nrpe plugin folder.

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -18,8 +18,18 @@ class dhrep::tools {
   dariahcommon::nagios_service { 'check_cron':
     command => "/usr/lib/nagios/plugins/check_procs -c 1:20 -w 1:2 -C cron",
   }
+
+  file { '/usr/lib/nagios/plugins/check_tivoli':
+    source  => 'puppet:///modules/dhrep/usr/lib/nagios/plugins/check_tivoli',
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => Class[dariahcommon::nagios],
+  }
   dariahcommon::nagios_service { 'check_tivoli_root':
     command => "/usr/lib/nagios/plugins/check_tivoli -c /",
+    require => File['/usr/lib/nagios/plugins/check_tivoli'],
   }
 
 }
