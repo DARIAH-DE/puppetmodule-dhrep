6f6d56d6d949257cf76901fd1a3f39cb250768d5
Stefan E. Funk <funk@sub.uni-goettingen.de>
2016-04-11 18:56:10 +0200

    scripts corrected.

--- files/opt/dhrep/check_ldap_backups.sh
+++ files/opt/dhrep/check_ldap_backups.sh
@@ -8,11 +8,8 @@ twodays=$((2*$oneday))
 # Taken from: http://stackoverflow.com/questions/1819187/test-a-file-date-with-bash (THANKS!)
 
 # Test backup
-if [ -f $P1 ]; then
-  echo "ldap backup file $P1 is existing"
-  echo "ok"
-else
-  echo "critical: ldap backup file $P1 does not exist!"
+if ! [ -f $P1 ]; then
+  echo "LDAP backup CRITICAL: LDAP backup file $P1 does not exist!"
   exit 2
 fi
 
@@ -20,21 +17,16 @@ modsecs=$(date --utc --reference=$P1 +%s)
 nowsecs=$(date +%s)
 delta=$(($nowsecs-$modsecs))
 
-echo "ldap backup file $P1 was modified $delta secs ago"
+#echo "ldap backup file $P1 was modified $delta secs ago"
 
 if [ $delta -gt $oneday ]; then
-  echo "critical: ldap backup file [$P1] is older than 24h!"
+  echo "LDAP backup CRITICAL: LDAP backup file [$P1] is older than 24h!"
   exit 2
-else
-  echo "ok"
 fi
 
 # Test backup of backup
-if [ -f $P2 ]; then
-  echo "ldap backup file $P2 is existing"
-  echo "ok"
-else
-  echo "warning: ldap backup file $P2 does not exist!"
+if ! [ -f $P2 ]; then
+  echo "LDAP backup WARNING: LDAP backup file $P2 does not exist!"
   exit 1
 fi
 
@@ -42,12 +34,12 @@ modsecs=$(date --utc --reference=$P2 +%s)
 nowsecs=$(date +%s)
 delta=$(($nowsecs-$modsecs))
 
-echo "ldap backup file $P2 was modified $delta secs ago"
+#echo "ldap backup file $P2 was modified $delta secs ago"
 
 if [ $delta -gt $twodays ]; then
-  echo "warning: ldap backup file [$P2] is older than 48h!"
+  echo "LDAP backup WARNING: LDAP backup file [$P2] is older than 48h!"
   exit 1
-else
-  echo "ok"
 fi
+
+echo "LDAP backup OK: backup files [$P1] and [$P2] existing"
 exit 0
--- files/opt/dhrep/check_ldap_statistics.sh
+++ files/opt/dhrep/check_ldap_statistics.sh
@@ -7,11 +7,8 @@ onemonth=$((31*24*60*60))
 # Taken from: http://stackoverflow.com/questions/1819187/test-a-file-date-with-bash (THANKS!)
 
 # Test statistic CSV file
-if [ -f $P1 ]; then
-  echo "ldap statistic $P1 is existing"
-  echo "ok"
-else
-  echo "critical: ldap statistic file $P1 does not exist!"
+if ! [ -f $P1 ]; then
+  echo "LDAP statistic CRITICAL: LDAP statistic file [$P1] does not exist!"
   exit 2
 fi
 
@@ -19,21 +16,16 @@ modsecs=$(date --utc --reference=$P1 +%s)
 nowsecs=$(date +%s)
 delta=$(($nowsecs-$modsecs))
 
-echo "ldap statistic file $P1 was modified $delta secs ago"
+#echo "ldap statistic file $P1 was modified $delta secs ago"
 
 if [ $delta -gt $onemonth ]; then
-  echo "critical: ldap statistic file [$P1] is older than one month!"
+  echo "LDAP statistics CRITICAL: LDAP statistic file [$P1] is older than one month!"
   exit 2
-else
-  echo "ok"
 fi
 
 # Test statistic TXT file
-if [ -f $P2 ]; then
-  echo "ldap statistic $P2 is existing"
-  echo "ok"
-else
-  echo "critical: ldap statistic file $P2 does not exist!"
+if ! [ -f $P2 ]; then
+  echo "LDAP statistics CRITICAL: LDAP statistic file [$P2] does not exist!"
   exit 2
 fi
 
@@ -41,13 +33,12 @@ modsecs=$(date --utc --reference=$P2 +%s)
 nowsecs=$(date +%s)
 delta=$(($nowsecs-$modsecs))
 
-echo "ldap statistic file $P2 was modified $delta secs ago"
+#echo "ldap statistic file $P2 was modified $delta secs ago"
 
 if [ $delta -gt $onemonth ]; then
-  echo "critical: ldap statistic file [$P2] is older than one month!"
+  echo "LDAP statistics CRITICAL: LDAP statistic file [$P2] is older than one month!"
   exit 2
-else
-  echo "ok"
 fi
 
+echo "LDAP statistics OK: statistic files [$P1] and [$P2] existing"
 exit 0
