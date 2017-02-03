fd1799dff7e2e965b3d8234a5bf7b65ac1afc06e
Stefan E. Funk <funk@sub.uni-goettingen.de>
2017-02-03 17:24:06 +0100

    corrected dhrep-doc.

--- manifests/static/repository_de_dariah_eu.pp
+++ manifests/static/repository_de_dariah_eu.pp
@@ -12,30 +12,22 @@ class dhrep::static::repository_de_dariah_eu(
   }
 
   #
-  # Create repository folder in /var/www
-  #
-  file { '/var/www/nginx-root/repository.de.dariah.eu':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  ->
-
-  #
   # DARIAH-DE Repository Documentation (common index file and folder)
   #
-  file { '/var/www/nginx-root/repository.de.dariah.eu/doc':
+  file { '/var/www/nginx-root/doc':
     ensure  => directory,
     owner   => root,
     group   => root,
     mode    => '0755',
   }
-  file { '/var/www/nginx-root/repository.de.dariah.eu/doc/index.html':
-    source  => 'puppet:///modules/dhrep/var/www/nginx-root/repository.de.dariah.eu/doc/index.html',
-    mode    => '0644',
-    require => File['/var/www/nginx-root/repository.de.dariah.eu/doc'],
-  }
+#
+# TODO Make index file for repository later.
+#
+#  file { '/var/www/nginx-root/doc/index.html':
+#    source  => 'puppet:///modules/dhrep/var/www/nginx-root/doc/index.html',
+#    mode    => '0644',
+#    require => File['/var/www/nginx-root/doc'],
+# }
 
   #
   # TextGridRep API Documentation DEB package (Sphinx-based)
@@ -45,7 +37,7 @@ class dhrep::static::repository_de_dariah_eu(
     require => Exec['update_dariah_apt_repository'],
   }
   ->
-  file { '/var/www/nginx-root/repository.de.dariah.eu/doc/services':
+  file { '/var/www/nginx-root/doc/services':
     ensure => link,
     target => '/var/www/doc/services',
   }
