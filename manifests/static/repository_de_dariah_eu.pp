5788dbec33fe86def69045d70db2f9693bd787fa
Stefan E. Funk <funk@sub.uni-goettingen.de>
2017-08-28 13:36:21 +0200

    Added dhrep bagit profile to /var/www/.

--- manifests/static/repository_de_dariah_eu.pp
+++ manifests/static/repository_de_dariah_eu.pp
@@ -15,14 +15,15 @@ class dhrep::static::repository_de_dariah_eu(
   # DARIAH-DE Repository Documentation (common index file and folder)
   #
   file { '/var/www/nginx-root/doc':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/var/www/nginx-root'],
   }
 
   #
-  # index.html file for repository.
+  # index.html file for repository
   #
   file { '/var/www/nginx-root/index.html':
     source  => 'puppet:///modules/dhrep/var/www/nginx-root/dariah/index.html',
@@ -31,14 +32,47 @@ class dhrep::static::repository_de_dariah_eu(
   }
 
   #
-  # TextGridRep API Documentation DEB package (Sphinx-based)
+  # DARIAH-DE API Documentation DEB package (Sphinx-based)
   #
   package { 'dariah-de-doc':
     ensure  => latest,
     require => Exec['update_dariah_apt_repository'],
   }
   -> file { '/var/www/nginx-root/doc/services':
-    ensure => link,
-    target => '/var/www/doc/services',
+    ensure  => link,
+    target  => '/var/www/doc/services',
+    require => File['/var/www/nginx-root/doc'],
+  }
+
+  #
+  # BagIt Profile folders and profile itself
+  #
+  file { '/var/www/nginx-root/schemas':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/var/www/nginx-root'],
+  }
+  file { '/var/www/nginx-root/schemas/bagit':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/var/www/nginx-root/schemas'],
+  }
+  file { '/var/www/nginx-root/schemas/bagit/profiles':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/var/www/nginx-root/schemas/bagit'],
+  }
+  file { '/var/www/nginx-root/schemas/bagit/profiles/dhrep.json':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('dhrep/var/www/nginx-root/dariah/schemas/bagit/profiles/dhrep.json.erb'),
   }
 }
