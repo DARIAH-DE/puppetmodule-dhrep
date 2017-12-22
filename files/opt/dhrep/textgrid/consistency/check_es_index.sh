c70f7af2cd534b4104de46d91b9ee03bc188a294
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2017-12-22 12:52:36 +0100

    add es index consistency check

---
+++ files/opt/dhrep/textgrid/consistency/check_es_index.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+LIMIT=50
+
+# find all elasticsearch entries where the internal id (._id) does not match the 
+# id in json metadata (textgridUri without the 'textgrid:'-prefix).
+# needs the idMatchesTextgridUri.groovy script in elasticsearch script dir.
+function run_query {
+  local JSON=`curl --request POST \
+    --silent \
+    --url http://localhost:9202/_search \
+    --header 'content-type: application/json' \
+    --data '{
+    "query": {
+      "filtered" : {
+        "query" : {
+          "match_all": {}
+        },
+        "filter" : {
+          "script" : {
+            "lang": "groovy",
+            "script_file": "idMatchesTextgridUri"
+          }
+        }
+      }
+    },
+    "from": 0,
+    "size": ${LIMIT},
+    "fields": ["textgridUri"]
+  }'`
+  echo $JSON
+}
+
+function idsFromJSON {
+  echo $1 | jq -r '.hits.hits[] | ._id'
+}
+
+case "$1" in
+  nagios)
+    LIMIT=1 # only the hits field is interesting for this test
+    JSON=$(run_query)
+    # https://www.digitalocean.com/community/tutorials/how-to-create-nagios-plugins-with-bash-on-ubuntu-12-10
+    HITS=`echo $JSON| jq .hits.total`
+    if ((HITS > 0)); then
+      echo "CRITICAL - found $HITS entries in elasticsearch where ID does not match textgridUri"
+      exit 2
+    fi
+    echo "OK - IDs and textgridUris in elasticsearch do all match"
+      exit 0
+    ;;
+  all)
+    JSON=$(run_query)
+    echo $JSON | jq .
+    ;;
+  uris)
+    JSON=$(run_query)
+    echo $(idsFromJSON $JSON)
+    ;;
+  hdcheck)
+    JSON=$(run_query)
+    for uri in $(idsFromJSON $JSON); do
+      echo "${uri} has no / wrong metadata in elasticsearch"
+      ../inspect-tgobject.sh $uri
+      echo "---"
+    done
+    ;;
+  *)
+    echo "no param1 given. usage: ${0} PARAM"
+    echo "PARAMs:"
+    echo "nagios - run as nagios check"
+    echo "all - show elasticsearch response for query (limit in query is ${LIMIT})"
+    echo "uris - show only broken uris (limit in query is ${LIMIT})"
+    echo "hdcheck - for each broken uri test if it exists in storage"
+    ;;
+esac
+
--- manifests/tools/cli.pp
+++ manifests/tools/cli.pp
@@ -14,27 +14,52 @@ class dhrep::tools::cli (
     ###
     # shell tools for repo inspection
     ###
-    file { '/opt/dhrep/functions.d':
+    file { "${_optdir}/functions.d":
       ensure  => directory,
       owner   => root,
       group   => root,
       mode    => '0755',
       require => File[$_optdir],
     }
-    file { '/opt/dhrep/functions.d/textgrid-shared.sh':
+    file { "${_optdir}/functions.d/textgrid-shared.sh":
       mode   => '0777',
       source => 'puppet:///modules/dhrep/opt/dhrep/textgrid/functions.d/textgrid-shared.sh',
-      require => File['/opt/dhrep/functions.d'],
+      require => File["${_optdir}/functions.d"],
     }
-    file { '/opt/dhrep/functions.d/inspect.sh':
+    file { "${_optdir}/functions.d/inspect.sh":
       mode   => '0777',
       source => 'puppet:///modules/dhrep/opt/dhrep/textgrid/functions.d/inspect.sh',
-      require => File['/opt/dhrep/functions.d'],
+      require => File["${_optdir}/functions.d"],
     }
-    file { '/opt/dhrep/inspect-tgobject.sh':
+    file { "${_optdir}/inspect-tgobject.sh":
       mode   => '0777',
       source => 'puppet:///modules/dhrep/opt/dhrep/textgrid/inspect-tgobject.sh',
       require => File[$_optdir],
+    }
+
+    ###
+    # consistency check
+    ###
+    file { "${_optdir}/consistency":
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      require => File[$_optdir],
+    }
+    file { "${_optdir}/consistency/check_es_index.sh":
+      mode   => '0777',
+      source => 'puppet:///modules/dhrep/opt/dhrep/textgrid/consistency/check_es_index.sh',
+      require => [File["${_optdir}/consistency"],File['/etc/elasticsearch/masternode/scripts/idMatchesTextgridUri.groovy']],
+    }
+    # the es-index consistency check needs a groovy script to be present in elasticsearch
+    file { '/etc/elasticsearch/masternode/scripts/idMatchesTextgridUri.groovy':
+      ensure  => file,
+      owner   => 'elasticsearch',
+      group   => 'elasticsearch',
+      mode    => '0640',
+      source  => 'puppet:///modules/dhrep/etc/elasticsearch/masternode/scripts/idMatchesTextgridUri.groovy',
+      require => File['/etc/elasticsearch/masternode/scripts/'],
     }
 
     ###
