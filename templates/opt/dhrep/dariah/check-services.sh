128217c7233ed41ad1a3075925c8d90f9210737a
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-02-28 19:09:59 +0100

    fits path corrected

--- templates/opt/dhrep/dariah/check-services.sh
+++ templates/opt/dhrep/dariah/check-services.sh
@@ -163,7 +163,7 @@ fi
 #
 FILE="/opt/dhrep/output"
 echo "checking "$CSTR"fits core"$CNRM" (intern)"$TRN
-cd /home/tomcat-fits/1.2.0
+cd /home/tomcat-fits/fits-1.2.0
 ./fits.sh -i License.md -o $FILE
 URGL=`grep toolname=\"FITS\" $FILE`
 URGL=`echo ${URGL:85:5}`
