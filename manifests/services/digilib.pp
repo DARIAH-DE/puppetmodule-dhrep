0d6b64e6e82572e9beff5c4566d0fed9d1d4f979
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-07-16 14:54:49 +0200

    adaptations to module upgrades fixed some warnings added user and group for sesame and openrdf-workbench .war files

--- manifests/services/digilib.pp
+++ manifests/services/digilib.pp
@@ -160,7 +160,7 @@ class dhrep::services::digilib (
 
 # calculate critical and warning values from xmx for nrpe
 $xmx_in_byte = inline_template("<%
-        mem,unit = @_xmx.scan(/\d+|\D+/)
+        mem,unit = @_xmx.scan(/\\d+|\\D+/)
         mem = mem.to_f
         case unit
             when nil
