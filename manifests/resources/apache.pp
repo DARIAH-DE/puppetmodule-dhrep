5f2fe4c5452ae9d3e717935deebb9f126b5bf512
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-07-26 18:36:28 +0200

    using aai-proxy now!

--- manifests/resources/apache.pp
+++ manifests/resources/apache.pp
@@ -74,7 +74,7 @@ class dhrep::resources::apache (
     <Directory />
         Options FollowSymLinks
         AllowOverride None
-  </Directory>
+    </Directory>
   ",
     order   => 010,
   }
@@ -82,7 +82,7 @@ class dhrep::resources::apache (
   if $scope == 'textgrid' {
     concat::fragment{'apache_textgrid':
       target  => $defaultvhost,
-      content => "
+      content => '
     DocumentRoot /var/www
 
     <Directory /var/www/>
@@ -97,8 +97,13 @@ class dhrep::resources::apache (
         Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
         Require all granted
     </Directory>
-    ",
-      order   => 020,
+
+    # disable entityID forward to the SP\'s Login Handler, IdP is the proxy
+    RewriteEngine On
+    RewriteCond      %{QUERY_STRING}    (.*?)&entityID=.*
+    RewriteRule      /(.*)               /$1?%1         [R=302,NE,L]
+    ',
+      order   => 100,
     }
   }
 
