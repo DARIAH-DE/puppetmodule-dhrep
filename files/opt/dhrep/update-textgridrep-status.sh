995327b7f151d001dbe22438658b2000f4c478be
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2018-07-26 17:54:05 +0200

    use template for update-textgridrep-status.sh as its also used by profiles::liferay with different paths

--- files/opt/dhrep/update-textgridrep-status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -N https://dariah-de.github.io/status/textgrid/repstatus.html -O /var/www/nginx-root/textgridlab.org/repstatus.html
-wget -N https://dariah-de.github.io/status/textgrid/repstatus.css -O /var/www/nginx-root/textgridlab.org/repstatus.css
-
-
