2adfe010d59eaca503f0ca264b0f4734a3437aed
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-08-02 18:32:45 +0200

    addind check-services nrpe probe to scripts.pp

--- manifests/tools/scripts.pp
+++ manifests/tools/scripts.pp
@@ -14,6 +14,8 @@ class dhrep::tools::scripts (
   $pid_port     = $::dhrep::params::config['tomcat_pid']['http_port'];
   $oaipmh_port  = $::dhrep::params::config['tomcat_oaipmh']['http_port'];
   $fits_port    = $::dhrep::params::config['tomcat_fits']['http_port'];
+  # using crud user for checking services, not user root
+  $crud_user    = $::dhrep::params::config['tomcat_crud']['user'];
 
   file { "${_optdir}/check-services.sh" :
     content => template("dhrep/opt/dhrep/${scope}/check-services.sh.erb"),
@@ -32,5 +34,13 @@ class dhrep::tools::scripts (
       mode    => '0755',
       require => File[$_optdir],
     }
+  }
+
+  ###
+  # nrpe for tgcrud
+  ###
+  nrpe::plugin { 'check_services':
+    plugin     => 'check-services.sh &> /dev/null',
+    libexecdir => $_optdir,
   }
 }
