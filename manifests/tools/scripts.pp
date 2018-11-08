f10f2a1a93c059e32365b374defd7d2b6789b2d4
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-11-08 16:01:20 +0100

    removed bash return code underdrückung in check_services script

--- manifests/tools/scripts.pp
+++ manifests/tools/scripts.pp
@@ -40,7 +40,7 @@ class dhrep::tools::scripts (
   # nrpe for tgcrud
   ###
   nrpe::plugin { 'check_services':
-    plugin     => 'check-services.sh > /dev/null 2>&1',
+    plugin     => 'check-services.sh > /dev/null',
     libexecdir => $_optdir,
   }
 }
