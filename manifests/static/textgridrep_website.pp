daf04e7d05b04b225e050f9b74bb03558939582d
Stefan E. Funk <funk@sub.uni-goettingen.de>
2018-11-15 16:03:48 +0100

    removing nginx config for old textgridrep website

--- manifests/static/textgridrep_website.pp
+++ manifests/static/textgridrep_website.pp
@@ -1,10 +1,11 @@
 # == Class: dhrep::static::textgridrep_website
 #
-# Class to install and configure the textgridrep website
+# Class to install and configure the (old) textgridrep website
 #
 class dhrep::static::textgridrep_website (
   $tgrep_server_name  = 'vm1rep vm1rep.textgrid.local',
   $tgrep_service_url  = 'http://vm1.textgrid.local/1.0',
+  $hide_site          = true,
 ) {
 
   include dhrep::nginx
@@ -13,57 +14,76 @@ class dhrep::static::textgridrep_website (
     path => ['/usr/bin','/bin','/usr/sbin'],
   }
 
-  file { '/var/www/nginx-root/textgridrep.de':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-  -> exec { 'git_clone_textgridrep':
-    command => 'git clone git://projects.gwdg.de/dariah-de/tg/textgrid-repository/textgridrep-webseite.git /var/www/nginx-root/textgridrep.de/textgridrep-webseite',
-    creates => '/var/www/nginx-root/textgridrep.de/textgridrep-webseite',
-    require => Package['git'],
-  }
-  -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox':
-    source  => 'file:///var/www/nginx-root/textgridrep.de/textgridrep-webseite',
-    recurse => true,
-  }
-  -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite/sandbox':
-    ensure => 'link',
-    target => '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox',
-  }
-  -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite/js/config.js':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('dhrep/var/www/nginx-root/textgridrep-webseite/config.js.erb'),
-  }
-  -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox/js/config.js':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('dhrep/var/www/nginx-root/textgridrep-webseite-sandbox/config.js.erb'),
-  }
+  # the old textgridrep website is hidden (and not installed) by default!
+  if ($hide_site) {
 
-  file { '/etc/nginx/sites-available/textgridrep':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('dhrep/etc/dhrep/nginx/sites-available/textgridrep.erb'),
-  }
-  -> file { '/etc/nginx/proxyconf/textgridrep.common.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('dhrep/etc/dhrep/nginx/proxyconf/textgridrep.common.conf.erb'),
-  }
-  -> file { '/etc/nginx/sites-enabled/textgridrep':
-    ensure => 'link',
-    source => '/etc/nginx/sites-available/textgridrep',
-    notify => Service['nginx'],
+    # just remove nginx config
+    file { '/etc/nginx/sites-available/textgridrep':
+      ensure  => absent,
+    }
+    -> file { '/etc/nginx/proxyconf/textgridrep.common.conf':
+      ensure  => absent,
+    }
+    -> file { '/etc/nginx/sites-enabled/textgridrep':
+      ensure => absent,
+      notify => Service['nginx'],
+    }
+
+  } else {
+
+    # install everything
+    file { '/var/www/nginx-root/textgridrep.de':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+    }
+    -> exec { 'git_clone_textgridrep':
+      command => 'git clone git://projects.gwdg.de/dariah-de/tg/textgrid-repository/textgridrep-webseite.git /var/www/nginx-root/textgridrep.de/textgridrep-webseite',
+      creates => '/var/www/nginx-root/textgridrep.de/textgridrep-webseite',
+      require => Package['git'],
+    }
+    -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox':
+      source  => 'file:///var/www/nginx-root/textgridrep.de/textgridrep-webseite',
+      recurse => true,
+    }
+    -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite/sandbox':
+      ensure => 'link',
+      target => '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox',
+    }
+    -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite/js/config.js':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('dhrep/var/www/nginx-root/textgridrep-webseite/config.js.erb'),
+    }
+    -> file { '/var/www/nginx-root/textgridrep.de/textgridrep-webseite-sandbox/js/config.js':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('dhrep/var/www/nginx-root/textgridrep-webseite-sandbox/config.js.erb'),
+    }
+
+    file { '/etc/nginx/sites-available/textgridrep':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('dhrep/etc/dhrep/nginx/sites-available/textgridrep.erb'),
+    }
+    -> file { '/etc/nginx/proxyconf/textgridrep.common.conf':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('dhrep/etc/dhrep/nginx/proxyconf/textgridrep.common.conf.erb'),
+    }
+    -> file { '/etc/nginx/sites-enabled/textgridrep':
+      ensure => 'link',
+      source => '/etc/nginx/sites-available/textgridrep',
+      notify => Service['nginx'],
+    }
   }
 }
