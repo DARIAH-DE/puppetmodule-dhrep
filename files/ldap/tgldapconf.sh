4e64661fa624f80367c5f4ceca4874a048009e54
Stefan E. Funk <funk@sub.uni-goettingen.de>
2019-01-29 17:43:46 +0100

    added missing tgldap config script to init-databases

--- files/ldap/tgldapconf.sh
+++ files/ldap/tgldapconf.sh
@@ -7,10 +7,7 @@ mv /etc/ldap/slapd.d /etc/ldap/slapd.d.bak.$timestamp
 mv /var/lib/ldap/ /var/lib/ldap.bak.$timestamp
 mkdir /etc/ldap/slapd.d
 mkdir /var/lib/ldap
-#slapadd -l /tmp/ldap-cn-config.ldif
-# -n 0 ?
-slapadd -F /etc/ldap/slapd.d -l /tmp/ldap-cn-config.ldif -n 0
+slapadd -F /etc/ldap/slapd.d -l ./ldap-cn-config.ldif -n 0
 chown -R openldap:openldap /etc/ldap/slapd.d
 chown -R openldap:openldap /var/lib/ldap
 /etc/init.d/slapd start
-
