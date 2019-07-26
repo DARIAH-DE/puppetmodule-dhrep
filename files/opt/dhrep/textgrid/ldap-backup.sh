8d086b0d4e4e20f07d281059c661df16c737bd11
Bernd Schloer <bernd.schloer@gwdg.de>
2019-07-26 11:18:01 +0200

    enable gzip-compression

--- files/opt/dhrep/textgrid/ldap-backup.sh
+++ files/opt/dhrep/textgrid/ldap-backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Backup old backup, only if existing :-)
-if [ -a /var/dhrep/backups/ldap/ldap-backup.ldif ]; then
-  sudo mv /var/dhrep/backups/ldap/ldap-backup.ldif /var/dhrep/backups/ldap/ldap-backup_old.ldif
+if [ -a /var/dhrep/backups/ldap/ldap-backup.ldif.gz ]; then
+  sudo mv /var/dhrep/backups/ldap/ldap-backup.ldif.gz /var/dhrep/backups/ldap/ldap-backup_old.ldif.gz
 fi
 
 # Create current backup
 sudo slapcat -l /var/dhrep/backups/ldap/ldap-backup.ldif
+sudo gzip /var/dhrep/backups/ldap/ldap-backup.ldif
