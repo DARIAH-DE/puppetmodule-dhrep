0b5e04a3606fae991511b4aac073f77436115cde
Stefan E. Funk <funk@sub.uni-goettingen.de>
2021-08-23 15:21:47 +0200

    Add CR submission

--- templates/opt/dhrep/dariah/re-index-dhrep-cr.sh
+++ templates/opt/dhrep/dariah/re-index-dhrep-cr.sh
@@ -6,10 +6,14 @@
 
 HOST=$1;
 DEST=$HOST"_cr_entries.xml";
+HANDLE_URL="hdl.handle.net";
+MAP=$HOST"_cr_map.txt";
 TMP1=$HOST"_1.tmp";
 TMP2=$HOST"_2.tmp";
 TMP3=$HOST"_3.tmp";
 TMP4=$HOST"_4.tmp";
+TMP5=$HOST"_5.tmp";
+TMP6=$HOST"_6.tmp";
 
 ###
 # METHODS
@@ -17,10 +21,7 @@ TMP4=$HOST"_4.tmp";
 
 # Delete temporary files
 function delete_temp_files () {
-    rm -f $TMP1
-    rm -f $TMP2
-    rm -f $TMP3
-    rm -f $TMP4
+    rm -f $TMP1, $TMP2, $TMP3, $TMP4, $TMP5, $TMP6;
 }
 
 ###
@@ -39,9 +40,13 @@ echo "---> HOSTNAME: $HOST";
 
 delete_temp_files;
 
-# Check for existing output file
+# Check for existing output and mapping file
 if [ -f "$DEST" ]; then
     echo "---> ERROR! Output file already exists: $DEST!"
+    exit 1;
+fi
+if [ -f "$MAP" ]; then
+    echo "---> ERROR! Mapping file already exists: $MAP!"
     exit 1;
 fi
 
@@ -49,9 +54,9 @@ echo "---> OUTPUT FILE: $DEST";
 
 # 1. We need all collections from DH-rep visa OAI-PMH: <https://$HOST/1.0/oaipmh/oai?verb=ListSets>
 echo -n "---> Getting all sets from OAI-PMH... ";
-# listsets1.tmp: OAI-PMH ListSets response
+# $TMP1: OAI-PMH ListSets response
 curl -XGET -s "https://$HOST/1.0/oaipmh/oai?verb=ListSets" > $TMP1;
-# listsets3.tmp: All Handle PIDs only
+# $TMP2: All Handle PIDs only
 xmllint --xpath "//*[local-name()='setSpec']/text()" $TMP1 > $TMP2
 declare -i SETS=$(wc -l < $TMP2)
 echo "FOUND $SETS SETS";
@@ -61,10 +66,12 @@ echo "---> Getting metadata for each set";
 COUNT=0
 while IFS= read -r PID; do
     let COUNT=COUNT+1
-    echo -n "---> ($COUNT/$SETS) Processing $PID... ";
+    echo "---> ($COUNT/$SETS) Processing $PID";
+    # Strip hdl:
+    PID=${PID/hdl:/};
     # For each collection (OAI-PMH set), get record metadata: <https://$HOST/1.0/oaipmh/oai?verb=GetRecord&identifier=hdl:21.T11991/0000-001B-4E7C-2&metadataPrefix=oai_dc>
-    echo -n "PASS#1... "
-    # listsets4.tmp: Single OAI-PMH record
+    echo -n "    ---> PASS#1: Get OAI-PMH metadata... "
+    # $TMP3: Single OAI-PMH record
     curl -XGET -s "https://$HOST/1.0/oaipmh/oai?verb=GetRecord&identifier=$PID&metadataPrefix=oai_dc" > $TMP3;
     IDENTIFIER_CR=$(xmllint --xpath "//*[local-name()='dc']/*[local-name()='identifier'][starts-with(text(),'hdl:')]" $TMP3);
     IDENTIFIER_CR=${IDENTIFIER_CR/hdl:/};
@@ -78,29 +85,50 @@ while IFS= read -r PID; do
     FORMAT=$(xmllint --xpath "//*[local-name()='dc']/*[local-name()='format']" $TMP3);
     TITLE=$(xmllint --xpath "//*[local-name()='dc']/*[local-name()='title']" $TMP3);
     IDENTIFIER_DOI=$(xmllint --xpath "//*[local-name()='dc']/*[local-name()='identifier'][starts-with(text(),'doi:')]" $TMP3);
+    echo "DONE";
+
     # Get contributor from Handle metadata: <https://hdl.handle.net/api/handles/21.T11991/0000-001B-4E7C-2?type=RESPONSIBLE>
-    echo -n "PASS#2... "
-    # listsets5.tmp: JSON file from Handle server for each PID
-    curl -XGET -s "https://hdl.handle.net/api/handles/21.T11991/0000-001B-4E7C-2?type=RESPONSIBLE" > $TMP4;
+    echo -n "    ---> PASS#2: Get Handle metadata... "
+    # $TMP4: JSON file from Handle server for each PID
+    curl -XGET -s "https://$HANDLE_URL/api/handles/$PID?type=RESPONSIBLE" > $TMP4;
     CONTRIBUTOR="<dc:contributor>"$(jq .values[].data.value $TMP4)"</dc:contributor";
     CONTRIBUTOR=${CONTRIBUTOR/\"/}
     CONTRIBUTOR=${CONTRIBUTOR/\"/}
-    # For each collection, build the CR request body from metadata. Write it to one file for Tobi (for the moment :-)
-    echo "" >> $DEST;
-    echo "<rdf:RDF xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">" >> $DEST;
-    echo "  <rdf:Description rdf:about=\"$DOI_WITH_RESOLVER\">" >> $DEST;
-    echo "        $IDENTIFIER_CR" >> $DEST;
-    echo "        $CREATOR" >> $DEST;
-    echo "        $RIGHTS" >> $DEST;
-    echo "        $IDENTIFIER_HDL" >> $DEST;
-    echo "        $FORMAT" >> $DEST;
-    echo "        $CONTRIBUTOR" >> $DEST;
-    echo "        $TITLE" >> $DEST;
-    echo "        $IDENTIFIER_DOI" >> $DEST;
-    echo "    </rdf:Description>" >> $DEST;
-    echo "</rdf:RDF>" >> $DEST;
     echo "DONE";
-    # TODO Later send it to <https://$HOST/colreg-ui/api/collections/submitAndPublish> automatically!
+
+    # For each collection, build the CR request body from metadata. Also write it to DEST file.
+    echo -n "    ---> PASS#3: Create and write CR submission info... ";
+    # $TMP5: Information for CR submission
+    echo "" > $TMP5;
+    echo "<rdf:RDF xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">" >> $TMP5;
+    echo "  <rdf:Description rdf:about=\"$DOI_WITH_RESOLVER\">" >> $TMP5;
+    echo "        $IDENTIFIER_CR" >> $TMP5;
+    echo "        $CREATOR" >> $TMP5;
+    echo "        $RIGHTS" >> $TMP5;
+    echo "        $IDENTIFIER_HDL" >> $TMP5;
+    echo "        $FORMAT" >> $TMP5;
+#    echo "        $CONTRIBUTOR" >> $TMP5;
+    echo "        <dc:contributor>StefanFunk@dariah.eu</dc:contributor>" >> $TMP5;
+    echo "        $TITLE" >> $TMP5;
+    echo "        $IDENTIFIER_DOI" >> $TMP5;
+    echo "    </rdf:Description>" >> $TMP5;
+    echo "</rdf:RDF>" >> $TMP5;
+    cat $TMP5 >> $DEST;
+    echo "DONE";
+
+    # Send it to <https://$HOST/colreg-ui/api/collections/submitAndPublish> automatically!
+    echo -n "    ---> PASS#4: Call CR#submitAndPublish() for collection creation... ";
+    if [ "$2" = "CR" ]; then
+        curl -XPOST -s "https://$HOST/colreg-ui/api/collections/submitDraft/" -H 'Content-Type: application/xml' -d @$TMP5 > $TMP6;
+        CRID=$(xmllint --xpath "//*[local-name()='collectionId']/text()" $TMP6);
+        echo "$PID $CRID" >> $MAP;
+        echo "DONE";
+    else
+        echo "SKIPPED";
+    fi
+
+    exit 2;
+
 done < $TMP2
 
 # TODO Get the Handle metadata SOURCE (ID used as redis key): <https://hdl.handle.net/api/handles/21.T11991/0000-001B-4E7C-2?type=SOURCE>
