d0347f7f0037d3f121bc73bf1dd39b0032792a65
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2021-10-17 18:41:55 +0200

    remove obsolete sesame-tomcat config

--- manifests/services/tomcat_sesame.pp
+++ manifests/services/tomcat_sesame.pp
@@ -6,7 +6,6 @@ class dhrep::services::tomcat_sesame (
   $scope = undef,
   $xmx   = $::dhrep::params::tomcat_sesame_xmx,
   $xms   = $::dhrep::params::tomcat_sesame_xms,
-  $use_tomcat = true,
 ) inherits dhrep::params {
 
   $catname      = $::dhrep::params::config['tomcat_sesame']['catname']
@@ -16,20 +15,4 @@ class dhrep::services::tomcat_sesame (
   $uid          = $::dhrep::params::config['tomcat_sesame']['uid']
   $gid          = $::dhrep::params::config['tomcat_sesame']['gid']
 
-  if $use_tomcat {
-    ###
-    # user, home-dir and user-tomcat
-    ###
-    usertomcat::instance { $catname:
-      uid              => $uid,
-      gid              => $gid,
-      http_port        => $http_port,
-      control_port     => $control_port,
-      jmx_port         => $jmx_port,
-      xmx              => $xmx,
-      xms              => $xms,
-      telegraf_enabled => true,
-      tomcat_version   => $dhrep::params::tomcat_version,
-    }
-  }
 }
